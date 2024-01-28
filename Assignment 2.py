02fc76e323bb978007298190830e45eb13e0971c
AmeeerMaviaKhan <157229386+AmeeerMaviaKhan@users.noreply.github.com>
2024-01-28 19:03:55 +0500

    Create Assignment 2.py

---
+++ Assignment 2.py
@@ -0,0 +1,6 @@
+# function with two arguments
+num1=float(input("Enter The No 1: "))
+num2=float(input("Enter The No 2: "))
+Operator=input("Enter The Operator: ")
+if Operator =='+':
+    print(num1+num2)
